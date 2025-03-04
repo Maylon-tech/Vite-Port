ed021b8e28ab8441349db91033771a2fc998f13f
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-03-04 10:52:07 +0900

    fixing some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import Home from './components/sections/Home'
 import About from './components/sections/About'
 import Projects from './components/sections/Projects'
 import Contact from './components/sections/Contact'
-
-import './App.css'
 import './index.css'
 
 function App() {
